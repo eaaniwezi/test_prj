a963f0f88159ddfdde459f78517317280a78041a
eaaniwezi <eaaninwezi@gmail.com>
2021-07-03 15:27:35 +0300

    completed test task

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -1,10 +1,175 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Screen1 extends StatelessWidget {
+class Screen1 extends StatefulWidget {
   const Screen1({Key? key}) : super(key: key);
 
   static const routeName = '/screen_1';
 
   @override
-  Widget build(BuildContext context) => Container();
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.grey,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  titleContainer(),
+                  bodyContainer(),
+                  testButton(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget testButton() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: GestureDetector(
+          onTap: () {
+            print("testButton");
+          },
+          child: Container(
+            width: 150,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.purple[400],
+              borderRadius: BorderRadius.circular(30),
+            ),
+            // ignore: prefer_const_constructors
+            child: Center(
+              // ignore: prefer_const_constructors
+              child: Text(
+                "TEST BUTTON",
+                // ignore: prefer_const_constructors
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget bodyContainer() {
+    return Expanded(
+      flex: 3,
+      child: Container(
+        // ignore: prefer_const_constructors
+        constraints: BoxConstraints.expand(),
+        color: Colors.grey[350],
+        // ignore: prefer_const_constructors
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          // ignore: prefer_const_constructors
+          child: Scrollbar(
+            isAlwaysShown: true,
+            // ignore: prefer_const_constructors
+            child: SingleChildScrollView(
+              // ignore: prefer_const_constructors
+              child: Text(
+                  // ignore: lines_longer_than_80_chars
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fug. t vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget titleContainer() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        // ignore: prefer_const_constructors
+        constraints: BoxConstraints.expand(),
+        // height: MediaQuery.of(context).size.height * 0.2,
+        child: Column(
+          children: [
+            // ignore: prefer_const_literals_to_create_immutables
+            textHeader(),
+            Flexible(
+              child: textBody(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textBody() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        // ignore: prefer_const_constructors
+        child: SingleChildScrollView(
+          // ignore: prefer_const_constructors
+          child: Text(
+              // ignore: lines_longer_than_80_chars
+              "Lorem ipsum dolor sit amet,Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"),
+        ),
+      ),
+    );
+  }
+
+  Widget textHeader() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.close_sharp,
+                color: Colors.white,
+              ),
+            ),
+            const Text(
+              "Title",
+              // ignore: prefer_const_constructors
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.close_sharp,
+                size: 35,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/screens/screen_2.dart
+++ lib/screens/screen_2.dart
@@ -1,10 +1,144 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+// ignore: prefer_relative_imports
+import 'package:test_prj/providers/horizontal_items.dart';
+// ignore: prefer_relative_imports
+import 'package:test_prj/providers/vertical_items.dart';
 
-class Screen2 extends StatelessWidget {
+// class Screen2 extends StatelessWidget {
+//   const Screen2({Key? key}) : super(key: key);
+
+//   static const routeName = '/screen_2';
+
+//   @override
+//   Widget build(BuildContext context) => Container();
+// }
+
+class Screen2 extends StatefulWidget {
   const Screen2({Key? key}) : super(key: key);
-
   static const routeName = '/screen_2';
 
   @override
-  Widget build(BuildContext context) => Container();
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                })
+          ],
+          backgroundColor: Colors.white,
+          elevation: 0,
+          // ignore: prefer_const_literals_to_create_immutables
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    'Section 1 (2)',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                _horizontalItem(),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Section 2 (4)',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                _verticalItem(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+   Widget _verticalItem() {
+    return ListView.builder(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        itemCount: Provider.of<HorizontalItemsProvider>(context).itemsCount,
+        itemBuilder: (context, index) {
+          var counter = Provider.of<VerticalItemsProvider>(
+            context,
+          ).generateItemAt(index);
+          return _item(counter);
+        });
+  }
+
+  Widget _item(String counter) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 2.0),
+      child: Card(
+        elevation: 6,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Container(
+          width: 150,
+          height: 100,
+          child: Center(
+            // ignore: prefer_const_constructors
+            child: Text(
+              '$counter',
+              // ignore: prefer_const_constructors
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _horizontalItem() {
+    return SizedBox(
+      height: 130.0,
+      width: double.infinity,
+      child: Center(
+        child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            physics: const ClampingScrollPhysics(),
+            itemCount: Provider.of<HorizontalItemsProvider>(context).itemsCount,
+            itemBuilder: (context, index) {
+              var counter = Provider.of<HorizontalItemsProvider>(
+                context,
+              ).generateItemAt(index);
+              return _item(counter);
+            }),
+      ),
+    );
+  }
 }
